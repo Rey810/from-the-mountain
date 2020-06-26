094f4506673e08195a93023503f09d8a77eabc17
reyvdb <Rey810@users.noreply.github.com>
2020-06-26 15:44:23 +0200

    adds page title

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,10 +1,3 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
@@ -37,6 +30,8 @@ function SEO({
       }
     `
   )
+
+  const pageTitle = title ? title : site.siteMetadata.title
   const metaDescription = description || site.siteMetadata.description
   const image =
     metaImage && metaImage.src
@@ -55,7 +50,7 @@ function SEO({
       htmlAttributes={{
         lang,
       }}
-      title={title}
+      title={pageTitle}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       link={
         canonical
