2e6fba597b7b6b17eaa0e7e28df1bb768401e4f6
reyvdb <Rey810@users.noreply.github.com>
2020-06-26 12:46:11 +0200

    adds attempted fix for mailto: body line break

--- src/components/images/avatar.js
+++ src/components/images/avatar.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const AvatarImage = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "avatar.jpg" }) {
+      placeholderImage: file(relativePath: { eq: "black.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
