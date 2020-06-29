bd56fb5d0b52170c2f18dba853e337de26ed06cb
reyvdb <Rey810@users.noreply.github.com>
2020-06-29 14:09:39 +0200

    adds negative margin for subscribe form

--- src/components/subscribe.js
+++ src/components/subscribe.js
@@ -9,7 +9,9 @@ export default function subscribe({ theme }) {
         theme === "dark" ? "dark" : "light"
       } connect-form flex flex-col sm:flex-row rounded-lg mx-auto px-4 pt-16 my-20 sm:w-10/12 md:w-2/3`}
     >
-      <div className={`subscribe-text p-8 flex flex-col w-full sm:w-1/2 mt-0`}>
+      <div
+        className={`subscribe-text p-8 -mb-px flex flex-col w-full sm:w-1/2 sm:-mr-px mt-0`}
+      >
         <h1 className="text-2xl font-medium">
           Want to See More Stuff Like This?
         </h1>
