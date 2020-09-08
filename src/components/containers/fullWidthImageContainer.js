effd88c05912f9c43749551bcdf2bf7e07007009
reyvdb <Rey810@users.noreply.github.com>
2020-09-08 15:19:51 +0200

    update g colours: add medium-dark to color-60

--- src/components/containers/fullWidthImageContainer.js
+++ src/components/containers/fullWidthImageContainer.js
@@ -7,11 +7,8 @@ import Img from "gatsby-image"
 // image width and height set to 100% to take up parent divs dimensions
 const fullWidthImageWrapper = props => {
   return (
-    <div className="relative bg-red-500 pb-1/2">
-      <Img
-        fluid={props.image.childImageSharp.fluid}
-        className="absolute-important h-full w-full object-cover"
-      />
+    <div className="relative bg-gray-700">
+      <Img fluid={props.image.childImageSharp.fluid} />
     </div>
   )
 }
