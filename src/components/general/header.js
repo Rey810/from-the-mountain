ce2f6444d73111082c210e1af6bc01a42159263e
reyvdb <Rey810@users.noreply.github.com>
2020-10-29 09:01:44 +0200

    update bg colours, align toolbox tools

--- src/components/general/header.js
+++ src/components/general/header.js
@@ -54,22 +54,13 @@ const Header = ({
 
   // adds specific header links if the header is the landing page header
   let additionalLinks = isPortfolioHeader ? (
-    <>
-      <Link
-        to="/blog"
-        className="header-blog-link opacity-90 hover:opacity-100 py-2 px-4 mx-2 font-semibold text-xs hidden sm:inline md:mx-8 "
-        target="_blank"
-      >
-        Blog
-      </Link>
-      <Link
-        to="#Connect"
-        className="contact-mail contact-button mx-auto header-contact-button center py-2 px-6 font-semibold shadow-md rounded-md"
-        target="_blank"
-      >
-        HIRE ME
-      </Link>
-    </>
+    <Link
+      to="#Connect"
+      className="contact-mail contact-button mx-auto header-contact-button center py-2 px-6 font-semibold shadow-md rounded-md"
+      target="_blank"
+    >
+      HIRE ME
+    </Link>
   ) : (
     <Link
       to="/#Connect"
